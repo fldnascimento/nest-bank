d4abc4bd657e86101b5593e89ca23ef116c602d7
Felipe Nascimento <felipeliman@hotmail.com>
2024-11-28 09:04:29 -0300

    fix: adjusted interface client.repository

--- src/domain/client/repositories/client.repository.ts
+++ src/domain/client/repositories/client.repository.ts
@@ -1,8 +1,6 @@
-import { Repo } from 'src/domain/common/infrastructure/repo';
+import { Repo } from 'src/domain/common/domain/repo';
 import { ClientEntity } from '../entities/client.entity';
 
 export interface ClientRepository extends Repo<ClientEntity> {
-  save(client: ClientEntity): Promise<ClientEntity>;
-  findById(id: string): Promise<ClientEntity | null>;
   findByCpf(cpf: string): Promise<ClientEntity | null>;
 }
